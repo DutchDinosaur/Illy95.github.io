6d5e25c93849f145a8d00229c000cad736904876
DutchDinosaur <carnplaynl@gmail.com>
2022-06-30 20:44:39 +0200

    added animation folder

--- gallery.js
+++ gallery.js
@@ -1,20 +1,31 @@
-var Scenery = ["BlissPicnic.gif", "prairie.gif", "SkeleFish.gif","transform.gif"];
-var Loaded = false;
+var Scenery = ["BlissPicnic.gif", "prairie.gif", "SkeleFish.gif"];
+var Animations = ["arisukiTransform.gif", "Ghosties.gif", "Skate.gif","archit.gif"];
+
 var galleryPiecePopout = document.getElementById("galleryPiecePopout");
 var popoutPiece = document.getElementById("popoutPiece");
 
-loadGallery();
+loadGallery(Scenery);
 
 if (getUrlVars().piece != null) {
     ExpandGalleryPiece(getUrlVars().piece);
 }
 
-function loadGallery() {
-    if (Loaded) return;
-    Loaded = true;
-    for (var i = 0; i < Scenery.length; i++) {
-        loadGalleryPiece(Scenery[i]);
+function clearGallery() {
+    document.getElementById("GalleryContent").innerHTML = '';
+}
+
+function loadGallery(pieces) {
+    clearGallery()
+    if (pieces != Animations) addGalleryFolders("Interactive");
+
+    if (pieces != Scenery) addGalleryFolders("Scenery");
+
+    for (var i = 0; i < pieces.length; i++) {
+        loadGalleryPiece(pieces[i]);
     }
+
+    if (pieces != Animations) addGalleryFolders("Animations");
+
 }
 
 function loadGalleryPiece(url) {
@@ -44,4 +55,37 @@ function ExpandGalleryPiece(url) {
 
 function CloseGalleryPiece() {
     galleryPiecePopout.style.display = "none";
+}
+
+function addGalleryFolders(type) {
+    var folder = document.createElement("div");
+    folder.setAttribute("class", "galleryPiece");
+    folder.addEventListener("click", function () {
+
+        switch (type) {
+            case "Interactive":
+                SwitchFullscreenApp('Works', true);
+                break;
+            case "Animations":
+                loadGallery(Animations);
+                break;
+            case "Scenery":
+                loadGallery(Scenery);
+                break;
+            default:
+        }
+
+    });
+
+    var Image = document.createElement("img");
+    Image.src = "UITextures/FolderWithFiles.png";
+    Image.style.height = "285px";
+    folder.appendChild(Image);
+
+    var Name = document.createElement("p");
+    var text = document.createTextNode(type);
+    Name.appendChild(text);
+    folder.appendChild(Name);
+
+    document.getElementById("GalleryContent").appendChild(folder);
 }
