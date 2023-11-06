47394d3b090a083c61539fc2919c1333924bc9fb
dutchdinosaur <carnplaynl@gmail.com>
2023-11-06 08:24:12 +0100

    jucebox test!

--- blog/webglTestScript.js
+++ blog/webglTestScript.js
@@ -1,31 +1,16 @@
-var vertexShaderSource = `
-    precision mediump float;
-    
-    attribute vec3 vertPosition;
-    attribute vec2 vertTexCoord;
 
-    varying vec2 fragTexCoord;
+var InitTest = function () {
 
-    uniform mat4 mWorld;
-    uniform mat4 mView;
-    uniform mat4 mProj;
+    loadTextResource('shaders/vertexShader.glsl', function (vsErr, vsText) {
+    loadTextResource('shaders/fragmentShader.glsl', function (fsErr, fsText) {
+    loadJSONResource( 'juce.json', function (modelerr, modelObject) {
+    loadImage( 'spitsDrink.png', function (imgerr, img) {
+        
+    runTest(vsText,fsText,modelObject,img);
+    }); }); }); });
+};
 
-    void main(){
-        fragTexCoord = vertTexCoord;
-        gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
-    }`;
-
-var fragmentShaderSource = `
-    precision mediump float;
-
-    varying vec2 fragTexCoord;
-    uniform sampler2D sampler;
-
-    void main(){
-        gl_FragColor = texture2D(sampler,fragTexCoord);
-    }`;
-
-var initTest = function () {
+var runTest = function (vertexShaderSource,fragmentShaderSource, juceModel,juceTecture) {
     var canvas = document.getElementById('webglCanvas');
     var gl = canvas.getContext('webgl');
     if (!gl) gl = canvas.getContext('experimental-webgl');
@@ -60,98 +45,50 @@ var initTest = function () {
         console.error('shader program validating error:', gl.getProgramInfoLog(program));
 
     //create buffer
-    var boxVertices = 
-	[ // X, Y, Z           U, V
-		// Top
-		-1.0, 1.0, -1.0,   0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
-		1.0, 1.0, 1.0,     1, 1,
-		1.0, 1.0, -1.0,    1, 0,
+    var Vertices = juceModel.meshes[0].vertices;
+    var VertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, VertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(Vertices), gl.STATIC_DRAW);
 
-		// Left
-		-1.0, 1.0, 1.0,    0, 0,
-		-1.0, -1.0, 1.0,   1, 0,
-		-1.0, -1.0, -1.0,  1, 1,
-		-1.0, 1.0, -1.0,   0, 1,
+    var Indices = [].concat.apply([], juceModel.meshes[0].faces);
+    var IndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(Indices), gl.STATIC_DRAW);
 
-		// Right
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,   0, 1,
-		1.0, -1.0, -1.0,  0, 0,
-		1.0, 1.0, -1.0,   1, 0,
-
-		// Front
-		1.0, 1.0, 1.0,    1, 1,
-		1.0, -1.0, 1.0,    1, 0,
-		-1.0, -1.0, 1.0,    0, 0,
-		-1.0, 1.0, 1.0,    0, 1,
-
-		// Back
-		1.0, 1.0, -1.0,    0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-		-1.0, -1.0, -1.0,    1, 1,
-		-1.0, 1.0, -1.0,    1, 0,
-
-		// Bottom
-		-1.0, -1.0, -1.0,   1, 1,
-		-1.0, -1.0, 1.0,    1, 0,
-		1.0, -1.0, 1.0,     0, 0,
-		1.0, -1.0, -1.0,    0, 1,
-	];
-    var boxVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-
-    var boxIndices = [
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
-    var boxIndexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
+    var TexCoords = juceModel.meshes[0].texturecoords[0];
+    var TexCoordBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, TexCoordBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(TexCoords), gl.STATIC_DRAW);
 
     //vertex attributes
+    gl.bindBuffer(gl.ARRAY_BUFFER, VertexBufferObject);
     var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     gl.vertexAttribPointer(
         positionAttribLocation, //location
         3, //number
         gl.FLOAT, // type
         gl.FALSE, //normalized
-        5 * Float32Array.BYTES_PER_ELEMENT, //size
+        3 * Float32Array.BYTES_PER_ELEMENT, //size
         0 //offset
     );
     gl.enableVertexAttribArray(positionAttribLocation);
 
-    var colorAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+    gl.bindBuffer(gl.ARRAY_BUFFER, TexCoordBufferObject);
+    var texCoordsAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
     gl.vertexAttribPointer(
-        colorAttribLocation, //location
+        texCoordsAttribLocation, //location
         2, //number
         gl.FLOAT, //type
         gl.FALSE, //normalized
-        5 * Float32Array.BYTES_PER_ELEMENT, //size
-        3 * Float32Array.BYTES_PER_ELEMENT //offset
+        2 * Float32Array.BYTES_PER_ELEMENT, //size
+        0 //offset
     );
-    gl.enableVertexAttribArray(colorAttribLocation);
+    gl.enableVertexAttribArray(texCoordsAttribLocation);
 
     //create texture
-    var boxTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+    var Texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, Texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
@@ -159,13 +96,12 @@ var initTest = function () {
     gl.texImage2D(
 		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
 		gl.UNSIGNED_BYTE,
-		document.getElementById('boxTexture')
+		juceTecture
 	);
     gl.bindTexture(gl.TEXTURE_2D, null);
 
     gl.useProgram(program);
 
-    
     //set shader matrecies
     var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
     var worldMatrix = new Float32Array(16);
@@ -174,7 +110,7 @@ var initTest = function () {
 
 	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
 	var viewMatrix = new Float32Array(16);
-	glMatrix.mat4.lookAt(viewMatrix, [0, 0, -8], [0, 1, 0], [0, 1, 0]);
+	glMatrix.mat4.lookAt(viewMatrix, [0, -.2, -1], [0, .2, 0], [0, 1, 0]);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 
 	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
@@ -191,18 +127,18 @@ var initTest = function () {
     var angle = 0;
     var loop = function () {
         angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-        glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0,1,0]);
-        glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1,0,0]);
-        glMatrix.mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
+        glMatrix.mat4.rotate(worldMatrix, identityMatrix, angle, [0,1,0]);
+        //glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 9, [0.1,0,0]);
+        //glMatrix.mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
         gl.clearColor(0, 0, 0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        gl.bindTexture(gl.TEXTURE_2D,boxTexture);
+        gl.bindTexture(gl.TEXTURE_2D,Texture);
         gl.activeTexture(gl.TEXTURE0);
 
-        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, Indices.length, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(loop);
     };
@@ -210,3 +146,4 @@ var initTest = function () {
 };
 
 //test with python3 -m http.server @ http://localhost:8000/webglTest.html
+//generate json models .\\assimp2json.exe 'source.fbx' 'destination.json'
